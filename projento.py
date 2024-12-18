59791f2843f4b33ad41382c7e4c08cf88918e32b
Igorthame <142543628+Igorthame@users.noreply.github.com>
2024-12-18 16:10:52 -0300

    api.py
    começo da minha api de livros em python

--- projento.py
+++ projento.py
@@ -1,1 +1,19 @@
-print("óla mundo")
+from flask import Flask, jsonify 
+
+app= Flask(__name__)
+
+livro=[
+    {
+    'id' :1,
+    'titulo': 'livro de receita',
+    'autor' :'erick jackar'
+    }
+]
+
+# consultar (todos)
+@app.route('/livro',methods=['GET'])
+def obter_livros():
+    return jsonify(livro)
+
+
+app.run(port=5000,host='localhost',debug=True)
